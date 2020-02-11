7595429758216695f44d90c35a4b0d3eee825322
Imran Herrmann <i.herrmann@basecom.de>
2020-02-11 10:20:10 +0100

    put functions in new file functions.c

---
+++ functions.h
@@ -0,0 +1,13 @@
+#ifndef FUNCTIONS_H_INCLUDED
+#define FUNCTIONS_H_INCLUDED
+
+int is_leapyear(int year);
+
+int get_days_for_month(int month, int year);
+
+int exists_date(int day, int month, int year);
+
+int day_of_the_year(int day, int month, int year);
+
+int input_date(int day, int month, int year);
+#endif // FUNCTIONS_H_INCLUDED
